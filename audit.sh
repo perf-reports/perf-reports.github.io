03128a2ed9e9f407328d65fc371e83927564f24b
bridgestew <bridgestew@gmail.com>
2018-03-08 16:54:07 -0500

    march 1 test

--- audit.sh
+++ audit.sh
@@ -6,13 +6,19 @@ NOW=$(date +"%m-%y")
 mkdir "$NOW"; chmod +w "$NOW"
 SECONDS=0
 
-# the home page didn't format nicely into the array/loop/echo, so it runs outside the loop on its own
-echo "Testing home"
+# the home and page not found page didn't format nicely into the array/loop/echo, so it runs outside the loop on its own
+echo "Testing: home"
 lighthouse --config-path=./vitamix-config.json https://www.vitamix.com --quiet --chrome-flags="--headless" --output html --output-path="$NOW/home.html"
 
 
+# the optimized product detail page format requires testing it on preview server since it isn't always in use on the site. the e310 product could be swapped out with any product, in reality.
+# big drawback to being able to automate this is the need to use OpenVPN to hit the page. for now, this page is tested maually
+#echo "Testing optimized pdp"
+#lighthouse --config-path=./vitamix-config.json https://vmx-preview.sparkred.com/shop/e310 --quiet --chrome-flags="--headless" --output html --output-path="$NOW/e310.html"
+
+
 # these are the pages to be tested, using their url path minus the root domain (country and language codes also excluded)
-PATHS="why-vitamix what-you-can-make find-recipes be-inspired support shop shop/blenders shop/ascent-series shop/legacy-series shop/personal-series shop/e310 shop/recommender"
+PATHS="why-vitamix what-you-can-make find-recipes be-inspired support shop shop/blenders shop/ascent-series shop/legacy-series shop/personal-series shop/recommender /page-not-found"
 
 # announces which page is being tested, creates a report with the page title for the file name and puts it in this month's directory
 for path in $PATHS; do
